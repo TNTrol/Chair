65ce4807a7a9a3da44f8bd690f99996c4d3bbc91
TNTrol <sem.go@bk.ru>
2019-09-05 00:20:59 +0300

    secondChang

--- semak.ino
+++ semak.ino
@@ -1,5 +1,5 @@
-
-int timeCount = 0, temp = 0, pin = -1;
+#define HIM 10
+int timeCount = 0, temp = 0, pin = -1, code = 0;
 
 void setup() {
     for(int i = 0; i < 12; i++)
@@ -15,9 +15,11 @@ void set(int a){
 } 
 
 void loop() {
-    timeCount = 2 * analogRead(A0);
+    code = analogRead(A0);
+    timeCount = 2 * code;
+    analogWrite(HIM, code / 4);
     if(temp >= timeCount){
-        pin++;
+        pin += (pin + 1 ) % HIM == 0 ? 2 : 1;
         set(pin);
         temp = 0;
     }
