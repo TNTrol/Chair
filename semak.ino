53f9e577126d6137f3549a8289288746c4c5d867
TNTrol <sem.go@bk.ru>
2019-09-12 00:10:05 +0300

    Теперь есть ограничитель

--- semak.ino
+++ semak.ino
@@ -1,27 +1,30 @@
 #define HIM 10
 #define SIZE 8
-int timeCount = 0, temp = 0, pin = -1, code = 0;
+int timeCount = 0, temp = 0, pin = -1, code = -1;
 
 void setup() {
     for(int i = 0; i < SIZE; i++)
-    pinMode(i,OUTPUT);
+        pinMode(i,OUTPUT);
 }
 
 void set(int a){
-    if(a < 0)
-        return;
     for(int i = 0; i < SIZE; i++)
         digitalWrite(i, LOW);
-    digitalWrite(a % SIZE, HIGH);
+    if(a >= 0)
+        digitalWrite(a % SIZE, HIGH);
 } 
 
 void loop() {
     code = analogRead(A0);
-    timeCount = 2 * code;
+    timeCount = code;
     analogWrite(HIM, code / 4);
-    if(temp >= timeCount){
-        set(++pin);
-        temp = 0;
+    if(code > 50){
+        if(temp >= timeCount){
+            set(++pin);
+            temp = 0;
+        }
+        temp++;
+    }else{
+     set(-1);   
     }
-    temp++;
 }
