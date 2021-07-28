9e0776b6a601235fa067fe90cb6e307f8c30dd1d
TNTrol <sem.go@bk.ru>
2021-07-29 01:03:40 +0300

    разложил по папкам, вынес все что можно в константы

--- lamps/lamps.ino
+++ lamps/lamps.ino
@@ -1,21 +1,26 @@
 #define HIM 10
-#define SIZE 8
+#define START_PIN 0
+#define END_PIN 8
+#define READ_PIN A0
+
+#define SIZE (END_PIN - START_PIN)
+
 int timeCount = 0, temp = 0, pin = -1, code = -1;
 
 void setup() {
-    for(int i = 0; i < SIZE; i++)
+    for(int i = START_PIN; i < END_PIN; i++)
         pinMode(i,OUTPUT);
 }
 
 void set(int a){
-    for(int i = 0; i < SIZE; i++)
+    for(int i = START_PIN; i < END_PIN; i++)
         digitalWrite(i, LOW);
     if(a >= 0)
         digitalWrite(a % SIZE, HIGH);
 } 
 
 void loop() {
-    code = analogRead(A0);
+    code = analogRead(READ_PIN);
     timeCount = code;
     analogWrite(HIM, code / 4);
     if(code > 50){
